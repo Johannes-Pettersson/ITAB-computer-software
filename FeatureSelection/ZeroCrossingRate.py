e7e1c320bd9b8ef14c15b23bd234112c98107f05
Tim Lundstedt <luti22bp@student.ju.se>
2025-03-17 11:32:48 +0100

    fix: cleanup, removed get_files() and replaced with manually entering path in main()

--- FeatureSelection/ZeroCrossingRate.py
+++ FeatureSelection/ZeroCrossingRate.py
@@ -5,42 +5,6 @@ import matplotlib.pyplot as plt
 import os
 import random
 
-# Parameters
-FRAME_SIZE = 1024
-HOP_SIZE = 512
-
-
-def get_files(num_of_good_files, num_of_faulty_files):
-    good_files = []
-    faulty_files = []
-
-    good_directories = [
-        "../Recording/Functioning_gate_recordings/Day 2/Session 1",
-        "../Recording/Functioning_gate_recordings/Day 2/Session 2",
-    ]
-    faulty_directories = [
-        "../Recording/Faulty_gate_recordings/Day 2/Session 1",
-        "../Recording/Faulty_gate_recordings/Day 2/Session 2",
-    ]
-
-    for dir in good_directories:
-        for entry in os.scandir(dir):
-            if entry.is_file():
-                good_files.append(entry.path)
-
-    for dir in faulty_directories:
-        for entry in os.scandir(dir):
-            if entry.is_file():
-                faulty_files.append(entry.path)
-
-    while len(good_files) > num_of_good_files:
-        good_files.pop(random.randrange(len(good_files)))
-
-    while len(faulty_files) > num_of_faulty_files:
-        faulty_files.pop(random.randrange(len(faulty_files)))
-
-    return good_files + faulty_files
-
 
 def calculate_values(file):
     """
@@ -70,23 +34,27 @@ def calculate_values(file):
 
         zcr_std : standard deviation of the zero crossing
     """
+    # Parameters
+    frame_size = 1024
+    hop_size = 512
     y, sr = librosa.load(file)
     zcr_total_val = sum(librosa.zero_crossings(y))
-    zcr = librosa.feature.zero_crossing_rate(y, frame_length=FRAME_SIZE, hop_length=HOP_SIZE)[0]
-    t = librosa.frames_to_time(np.arange(len(zcr)), sr=sr, hop_length=HOP_SIZE)
+    zcr = librosa.feature.zero_crossing_rate(y, frame_length=frame_size, hop_length=hop_size)[0]
+    t = librosa.frames_to_time(np.arange(len(zcr)), sr=sr, hop_length=hop_size)
     zcr_mean = np.mean(zcr)
     zcr_max = np.max(zcr)
     zcr_std = np.std(zcr)
     return y, sr, t, zcr_total_val, zcr, zcr_mean, zcr_max, zcr_std
 
-
-
 def main():
-    n_good_files = 2
-    n_faulty_files = 2
-    titles = get_files(n_good_files, n_faulty_files)
+    titles = [
+        "../Recording/Functioning_gate_recordings/Day 2/Session 1/G_G_1.WAV",
+        "../Recording/Functioning_gate_recordings/Day 2/Session 2/G_G_1.WAV",
+        "../Recording/Faulty_gate_recordings/Day 2/Session 1/B_G_1.WAV",
+        "../Recording/Faulty_gate_recordings/Day 2/Session 2/B_G_1.WAV",
+    ]
     audio_data = [calculate_values(file) for file in titles]
-    fig, axes = plt.subplots(n_good_files, n_faulty_files, figsize=(80, 60))
+    fig, axes = plt.subplots(2, 2)
     mng = plt.get_current_fig_manager()
     mng.full_screen_toggle()
     for ax, (y, sr, t, zcr_total_val, zcr, zcr_mean, zcr_max, zcr_std), title in zip(
