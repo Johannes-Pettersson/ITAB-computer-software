22e0328e27014230f21317854e61b37b2c8f035b
Johannes Pettersson <pejo22ev@student.ju.se>
2025-03-28 10:55:26 +0100

    Adding one-dim plotting for lof

--- System/LOF.py
+++ System/LOF.py
@@ -25,26 +25,46 @@ def calc_and_plot_lof(data: np.ndarray, data_point=None, n_neighbors=20):
 
     X_scores = estimator.negative_outlier_factor_
 
+    if data.shape[1] == 2:
+        if data_point is not None:
+            dot_colors = ["k" if i < len(labels)-1 else "g" for i in range(len(labels))]
+            plt.scatter(data[:, 0], data[:, 1], color=dot_colors, s=3.0)
+        else:
+            plt.scatter(data[:, 0], data[:, 1], color="k", s=3.0)
 
-    if data_point is not None:
-        dot_colors = ["k" if i < len(labels)-1 else "g" for i in range(len(labels))]
-        plt.scatter(data[:, 0], data[:, 1], color=dot_colors, s=3.0)
-    else:
-        plt.scatter(data[:, 0], data[:, 1], color="k", s=3.0)
+        perimiter_colors = [(1,0,0,0.5) if label == -1 else (0,0,0,0) for label in labels]
+        if perimiter_colors[-1] != (0,0,0,0):
+            perimiter_colors[-1] = (0,1,0,0.5)
 
-    perimiter_colors = [(1,0,0,0.5) if label == -1 else (0,0,0,0) for label in labels]
-    if perimiter_colors[-1] != (0,0,0,0):
-        perimiter_colors[-1] = (0,1,0,0.5)
+        radius = (X_scores.max() - X_scores) / (X_scores.max() - X_scores.min())
+        plt.scatter(
+            data[:, 0],
+            data[:, 1],
+            s=1000 * radius,
+            edgecolors=["r"if i < len(data)-1 else "g" for i in range(len(data))],
+            facecolors=perimiter_colors,
+            label="Outlier scores",
+        )
+    elif data.shape[1] == 1:
+        if data_point is not None:
+            dot_colors = ["k" if i < len(labels)-1 else "g" for i in range(len(labels))]
+            plt.scatter(data, np.zeros(len(data)), color=dot_colors, s=3.0)
+        else:
+            plt.scatter(data, np.zeros(len(data)), color="k", s=3.0)
 
-    radius = (X_scores.max() - X_scores) / (X_scores.max() - X_scores.min())
-    plt.scatter(
-        data[:, 0],
-        data[:, 1],
-        s=1000 * radius,
-        edgecolors=["r"if i < len(data)-1 else "g" for i in range(len(data))],
-        facecolors=perimiter_colors,
-        label="Outlier scores",
-    )
+        perimiter_colors = [(1,0,0,0.5) if label == -1 else (0,0,0,0) for label in labels]
+        if perimiter_colors[-1] != (0,0,0,0):
+            perimiter_colors[-1] = (0,1,0,0.5)
+
+        radius = (X_scores.max() - X_scores) / (X_scores.max() - X_scores.min())
+        plt.scatter(
+            data,
+            np.zeros(len(data)),
+            s=1000 * radius,
+            edgecolors=["r"if i < len(data)-1 else "g" for i in range(len(data))],
+            facecolors=perimiter_colors,
+            label="Outlier scores",
+        )
 
     plt.axis("tight")
     if data_point is not None:
