0d97dd3913e50a2f053dc03dbbfdc12dc4ff6140
Tim Lundstedt <luti22bp@student.ju.se>
2025-03-31 23:18:43 +0200

    feat: update to get files to retrieve files from new structured dataset directory

--- System/GetFiles.py
+++ System/GetFiles.py
@@ -1,7 +1,14 @@
 import os
 import random
 
-def get_files(num_of_good_gate_files, num_of_faulty_gate_files, pick_randomly=True):
+
+def get_files(
+    num_of_good_gate_files,
+    num_of_faulty_gate_files,
+    good_gate_dir=None,
+    faulty_gate_dir=None,
+    pick_randomly=True,
+):
     """
     Set pick_randomly to False to get the same files every time
 
@@ -13,15 +20,35 @@ def get_files(num_of_good_gate_files, num_of_faulty_gate_files, pick_randomly=Tr
     faulty_gate_files = []
 
     functioning_directories = [
-        "../Recording/Functioning_gate_recordings/Day 2/Session 1",
-        "../Recording/Functioning_gate_recordings/Day 2/Session 2"
+        (
+            (
+                "../Recording/Functioning_gate_recordings/Day 2/Session 1",
+                "../Recording/Functioning_gate_recordings/Day 2/Session 2",
+            )
+            if good_gate_dir is None
+            else good_gate_dir
+        )
     ]
     faulty_directories = [
-
-        "../Recording/Faulty_gate_recordings/Day 2/Session 1",
-        "../Recording/Faulty_gate_recordings/Day 2/Session 2"
-
+        (
+            (
+                "../Recording/Faulty_gate_recordings/Day 2/Session 1",
+                "../Recording/Faulty_gate_recordings/Day 2/Session 2",
+            )
+            if faulty_gate_dir is None
+            else faulty_gate_dir
+        )
     ]
+    print("-------------------")
+    print(functioning_directories)
+    print(faulty_directories)
+    print("-------------------")
+    if not os.path.isdir(functioning_directories[0]) or not os.path.isdir(
+        faulty_directories[0]
+    ):
+        raise ValueError("The directories do not exist")
+    else:
+        print("Directories exist")
 
     for dir in functioning_directories:
         for entry in os.scandir(dir):
@@ -33,10 +60,27 @@ def get_files(num_of_good_gate_files, num_of_faulty_gate_files, pick_randomly=Tr
             if entry.is_file():
                 faulty_gate_files.append(entry.path)
 
-    while(len(good_gate_files) >  num_of_good_gate_files):
-        good_gate_files.pop(random.randrange(len(good_gate_files))) if pick_randomly else good_gate_files.pop()
+    while len(good_gate_files) > num_of_good_gate_files:
+        (
+            good_gate_files.pop(random.randrange(len(good_gate_files)))
+            if pick_randomly
+            else good_gate_files.pop()
+        )
 
-    while(len(faulty_gate_files) > num_of_faulty_gate_files):
-        faulty_gate_files.pop(random.randrange(len(faulty_gate_files))) if pick_randomly else faulty_gate_files.pop()
+    while len(faulty_gate_files) > num_of_faulty_gate_files:
+        (
+            faulty_gate_files.pop(random.randrange(len(faulty_gate_files)))
+            if pick_randomly
+            else faulty_gate_files.pop()
+        )
+    if (
+        len(good_gate_files) != num_of_good_gate_files
+        and len(faulty_gate_files) != num_of_faulty_gate_files
+    ):
+        raise ValueError(
+            "Number of files in the directories is less than the number of files requested"
+        )
+
+    print("OK!!")
 
     return good_gate_files, faulty_gate_files
